e68b7756de70aad7ae694f3fae2e03415ccf666b
viktoriya maslennikova <libikvikaa@yandex.ru>
2020-11-24 19:42:17 +0500

    AddedTest

---
+++ src/main/java/ru/netology/radio.java
@@ -0,0 +1,105 @@
+package ru.netology;
+
+public class radio {
+    private int currentStation;
+    private int maxStation;
+    private int minStation;
+    private int currentVolume;
+    private int maxVolume;
+    private int minVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > getMaxStation() || currentStation < getMinStation()){
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > getMaxVolume() || currentVolume < getMinVolume() ){
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+
+
+    public void  increaseCurrentStation() {
+        if (currentStation >= maxStation ) {
+            currentStation = 0;
+            return;
+        }
+        this.currentStation ++;
+    }
+
+
+
+    public void  decreaseCurrentStation() {
+        if (currentStation <= minStation){
+            currentStation = 9;
+            return;
+        }
+            this.currentStation --;
+        }
+
+
+
+    public void  increaseCurrentVolume() {
+        if (currentVolume >= maxVolume) {
+            currentVolume = 10;
+            return;
+        }
+        this.currentVolume ++;
+    }
+
+
+
+    public void  decreaseCurrentVolume() {
+        if (currentVolume <= minVolume){
+            currentVolume = 0;
+            return;
+        }
+        this.currentVolume --;
+    }
+
+}
---
+++ src/test/java/ru/netology/radioTest.java
@@ -0,0 +1,57 @@
+package ru.netology;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class radioTest {
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "/dataForIncreaseStation.csv", delimiter = '|', numLinesToSkip = 2)
+    void shouldIncreaseCurrentStation(int currentStation, int minStation, int maxStation, int expected ) {
+        radio radio = new radio();
+        radio.setMaxStation(maxStation);
+        radio.setMinStation(minStation);
+        radio.setCurrentStation(currentStation);
+        radio.increaseCurrentStation();
+
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "/dataForDecreaseStation.csv" , delimiter = '|', numLinesToSkip = 2)
+    void shouldDecreaseCurrentStation(int currentStation, int minStation, int maxStation, int expected ) {
+        radio radio = new radio();
+        radio.setMaxStation(maxStation);
+        radio.setMinStation(minStation);
+        radio.setCurrentStation(currentStation);
+        radio.decreaseCurrentStation();
+
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "/dataForIncreaseVolume.csv", delimiter = '|', numLinesToSkip = 2)
+    void shouldIncreaseCurrentVolume(int currentVolume, int minVolume, int maxVolume, int expected) {
+        radio radio = new radio();
+        radio.setMaxVolume(maxVolume);
+        radio.setMinVolume(minVolume);
+        radio.setCurrentVolume(currentVolume);
+        radio.increaseCurrentVolume();
+
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "/dataForDecreaseVolume.csv", delimiter = '|', numLinesToSkip = 2)
+    void shouldDecreaseCurrentVolume(int currentVolume, int minVolume, int maxVolume, int expected) {
+        radio radio = new radio();
+        radio.setMaxVolume(maxVolume);
+        radio.setMinVolume(minVolume);
+        radio.setCurrentVolume(currentVolume);
+        radio.decreaseCurrentVolume();
+
+        assertEquals(expected, radio.getCurrentVolume());
+    }
+}
