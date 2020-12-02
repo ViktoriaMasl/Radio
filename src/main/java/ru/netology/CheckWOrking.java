63f5040e1a78b7f63c50c317ff3747d03e26e8d0
viktoriya maslennikova <libikvikaa@yandex.ru>
2020-12-02 20:45:12 +0500

    flex_try_again

--- src/main/java/ru/netology/CheckWOrking.java
+++ src/main/java/ru/netology/CheckWOrking.java
@@ -4,5 +4,6 @@ public class CheckWOrking {
     public static void checker() {
         System.out.println("Check");
         System.out.println("Check #2");
+        System.out.println("Check #3");
     }
 }
