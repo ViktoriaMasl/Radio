1184d396557af18e38ca20095d18c9ad51f4d438
viktoriya maslennikova <libikvikaa@yandex.ru>
2020-12-03 14:39:07 +0500

    finish version

--- src/main/java/ru/netology/CheckWOrking.java
+++
@@ -1,9 +0,0 @@
-package ru.netology;
-
-public class CheckWOrking {
-    public static void checker() {
-        System.out.println("Check");
-        System.out.println("Check #2");
-        System.out.println("Check #3");
-    }
-}
